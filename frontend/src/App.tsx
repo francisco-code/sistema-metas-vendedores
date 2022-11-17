970acae48606eb87b697298ec53b3c2f71d9a20d
Felipe Correia <felipedreamcorreia@hotmail.com>
2022-11-17 00:19:08 -0300

    componente - botão

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Front limpo e pronto para iniciarmos</h1>
+    <NotificationButton></NotificationButton>
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+import icon from '../../assets/img/vector-notification.svg'
+
+function NotificationButton() {
+    return (
+        <div className="sismeta-red-btn">
+            <img src={icon} alt="Ícone de notificação" />
+        </div>
+    )
+}
+
+export default NotificationButton

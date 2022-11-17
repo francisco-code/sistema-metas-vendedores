994ea2cd61cdb7e64b4bd7109b6520d113d283f9
Felipe Correia <felipedreamcorreia@hotmail.com>
2022-11-16 23:27:09 -0300

    project created

---
+++ backend/src/main/java/com/sismeta/sismeta/SismetaApplication.java
@@ -0,0 +1,13 @@
+package com.sismeta.sismeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sismeta/sismeta/SismetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sismeta.sismeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SismetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
